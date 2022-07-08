c7ced99956fc86eb63ac81833aa44d713ce62741
Philippe Covington <philippehcov@gmail.com>
2022-07-08 13:39:12 -0400

    Fin!

--- README.md
+++ README.md
@@ -12,6 +12,16 @@ Point scored if ball goes into goal.
 
 To 7 Points.
 
+Project Status: Finished
+
+Everything is functional
+Things I would like to Add:
+-Start/Restart Button
+-Win Logic
+-2 Players Function
+-AI Difficulty
+-Ball Speed Changer
+-Fix Bugs (1 Being Ball Gliding Across Player Paddle)
 
 
 ![image](https://user-images.githubusercontent.com/98479310/177248929-50a46889-b270-42a7-83ee-78fba4464ce8.png)
--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-const ball = document.getElementById('ball')
+let ball = document.getElementById('ball')
 let playerPaddle = document.getElementById('player-paddle')
-let compPaddle = document.getElementById('computer-paddle')
-const playerScore = document.getElementById('player-score')
-const compScore = document.getElementById('computer-score')
+let compPaddle = document.getElementById('comp-paddle')
 
 
-playerPaddle.style.marginLeft = '30px'
-playerPaddle.style.maginTop = '0px'
-compPaddle.style.marginLeft = '1048px'
+playerPaddle.style.marginLeft = '22.5px'
+playerPaddle.style.marginTop = '0px'
+compPaddle.style.marginLeft = '786px'
 compPaddle.style.marginTop = '0px'
-ball.style.marginLeft = '534px'
-ball.style.marginTop = '262px'
+ball.style.marginLeft = '400.5px'
+ball.style.marginTop = '196.5px'
 
-
-//////////////////////////////////////////////////////////////////
-///////////PADDLE SCRIPT///////////
 
 let W_Pressed = false
 let S_Pressed = false
+let  ID
+
+let Vx = -1
+let Vy = -1
+let V = Math.sqrt(Math.sqrt(Math.pow(Vx,2)+Math.pow(Vy,2)))
+
+
 
 document.addEventListener('keydown',(e)=> {
   if(e.keyCode=='87'){
@@ -37,17 +39,61 @@ document.addEventListener('keyup',(e)=> {
   }
 })
 
-
 gameLoop()
+
+function reset(){
+  clearInterval(ID)
+  Vx = 1
+  Vy = -1
+  V = Math.sqrt(Math.sqrt(Math.pow(Vx,2)+Math.pow(Vy,2)))
+  ball.style.marginLeft = '400.5px'
+  ball.style.marginTop = '196.5px'
+  gameLoop()
+}
 
 function gameLoop(){
   setTimeout(() =>{
-    setInterval(() =>{
-      if(W_Pressed && marginTop(playerPaddle)>-181){
-        playerPaddle.style.marginTop = `${marginTop(playerPaddle)-1}px`
+    ID = setInterval(() =>{
+      if(marginLeft(ball)<0){
+        document.querySelector('#comp-score').innerHTML = Number(document.querySelector('#comp-score').innerHTML)+1
+        reset()
+        return
+
       }
-      else if(S_Pressed && marginTop(playerPaddle)<181){
-        playerPaddle.style.marginTop = `${marginTop(playerPaddle)+1}px`
+      if((marginLeft(ball)+5) > 816){
+        document.querySelector('#player-score').innerHTML = Number(document.querySelector('#player-score').innerHTML)+1
+        reset()
+        return
+        
+      }
+      if(marginTop(ball)<0 || (marginTop(ball)+10)> 410){
+        Vy = -Vy
+      }
+
+      let paddle = (marginLeft(ball)+10<300) ? playerPaddle : compPaddle
+
+      if(collisionDetected(paddle)){
+        let angle
+        let type = (marginLeft(paddle) == 20) ? 'player' : 'comp'
+        if(ball.centerY==paddle.centerY){
+          angle = (type=='comp' ? 0 : Math.PI)
+        }
+      Vx = -Vx
+      }
+
+      let compLevel = 0.03
+compPaddle.style.marginTop = `${marginTop(compPaddle)+((ball.centerY - (marginTop(compPaddle)+ 18))) * compLevel}px`
+
+
+        ball.style.marginLeft = `${marginLeft(ball)+Vx}px`
+        ball.style.marginTop = `${marginTop(ball)+Vy}px`
+
+
+      if(W_Pressed && marginTop(playerPaddle)>0){
+        playerPaddle.style.marginTop = `${marginTop(playerPaddle)-0.8}px`
+      }
+      else if(S_Pressed && marginTop(playerPaddle)<355){
+        playerPaddle.style.marginTop = `${marginTop(playerPaddle)+0.8}px`
       }
     },5)
   },500)
@@ -55,6 +101,25 @@ function gameLoop(){
 //If 'W' is pressed wih each passing interval, decrement the value of marginTop of the player paddle by 2.
 //If 'S' is pressed wih each passing interval, increment the value of marginTop of the player paddle by 2.
 //Statements after && prevent paddle from going off page
+function collisionDetected(paddle){
+  ball.top = marginTop(ball)
+  ball.bottom = marginTop(ball) + 20
+  ball.left = marginLeft(ball)
+  ball.right = marginLeft(ball) + 20
+  ball.centerX = marginLeft(ball) + 10
+  ball.centerY = marginTop(ball) + 10
+
+  paddle.top = marginTop(paddle)
+  paddle.bottom = marginTop(paddle) + 50
+  paddle.left = marginLeft(paddle)
+  paddle.right = marginLeft(paddle) + 10
+  paddle.centerX = marginLeft(paddle)  + 5
+  paddle.centerY = marginTop(paddle)  + 36
+
+  return ball.left < paddle.right && ball.top < paddle.bottom && ball.right > paddle.left && ball.bottom > paddle.top
+}
+
+
 
 function marginTop(elem){
   return Number(elem.style.marginTop.split('p')[0])
@@ -67,5 +132,5 @@ function marginLeft(elem){
 //marginTop & marginLeft functions will return the numerical value of marginTop or marginLeft property of any element passed to them as an argument.
 
 
-//////////////////////////////////////////////////////////////////
-/////////////BALL SCRIPT///////////
+
+
